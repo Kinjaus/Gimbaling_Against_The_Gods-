acbefad35ff9962c7aba4242d716f94ee5e5d6ef
Kinjaus <kinjerskiaustin@gmail.com>
2024-12-09 00:46:44 -0800

    Update PlayerFallZone.cs

--- Coin_Game_Project/Assets/Scripts/Coin_Dozer/FallZone/PlayerFallZone.cs
+++ Coin_Game_Project/Assets/Scripts/Coin_Dozer/FallZone/PlayerFallZone.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class PlayerFallZone : _FallZone
 {
     // Start is called before the first frame update
+    
     public override void ResolveCoin(GameObject other)
     {
+        //HealthManager healthManager;
+        
         switch (other.tag)
         {
             case "copper":
+                //healthManager.DamageEnemy();
                 break;
             case "silver":
+                //healthManager.DamageEnemy();
                 break;
             case "gold":
+                //healthManager.DamageEnemy();
                 break;
             case "bad":
+                //healthManager.DamagePlayer();
                 break;
             default:
                 break;
